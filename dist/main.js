63a6aafa6e4fc93aa7fea2463e6674625a52b179
Stephen James <sajames1958@gmail.com>
2016-12-08 18:48:47 -0600

    tweeak

--- dist/main.js
+++ dist/main.js
@@ -1,7 +1,16 @@
-var Person = (function () {
-    function Person() {
-        this.name = "joe";
+"use strict";
+var _ = require('lodash');
+var Observable_1 = require('rxjs/Observable');
+require('rxjs/add/observable/timer');
+Observable_1.Observable.timer(1000, 1000)
+    .subscribe(function (x) { return console.log(x); });
+var App = (function () {
+    function App() {
+        this.title = "Eggheads";
     }
-    return Person;
+    App.prototype.getUsers = function () {
+        return [{ name: "John" }];
+    };
+    return App;
 }());
-;
+console.log(_.isArray(new App().getUsers()));
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,17 @@
-class Person{
-  name = "joe"
-};
+import {SocialNetwork} from './social-network';
+
+import * as _ from 'lodash';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/timer';
+
+Observable.timer(1000, 1000)
+  .subscribe(x => console.log(x));
+
+class App implements SocialNetwork {
+  title = "Eggheads";
+  getUsers() {
+    return [{name: "John"}];
+  }
+}
+
+console.log(_.isArray(new App().getUsers()));
